ee1e122edab57641b78be48ed4875a9886f5e82b
Rahul Anand Sahu <sahurahul442001@gmail.com>
2021-09-10 18:35:46 +0530

    added code editor features (autocomplete, tags etc)

--- src/components/Editor/editor.component.jsx
+++ src/components/Editor/editor.component.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import "./editor.styles.css";
+
+// code mirror playground
 import { Controlled as CodeMirror } from "react-codemirror2";
+
+// code mirror language features
 require("codemirror/mode/xml/xml");
 require("codemirror/mode/javascript/javascript");
 require("codemirror/mode/css/css");
+
+// code mirror properties
+require("codemirror/addon/hint/show-hint");
+require("codemirror/addon/hint/javascript-hint");
+require("codemirror/addon/hint/show-hint.css");
+require("codemirror/addon/hint/anyword-hint");
+require("codemirror/addon/edit/closebrackets");
+require("codemirror/addon/edit/closetag");
+require("codemirror/addon/fold/foldcode");
+require("codemirror/addon/fold/foldgutter");
+require("codemirror/addon/fold/brace-fold");
+require("codemirror/addon/fold/comment-fold");
+require("codemirror/addon/fold/foldgutter.css");
 
 const Editor = (props) => {
 	const { language, displayName, onChange, value } = props;
@@ -25,6 +42,15 @@ const Editor = (props) => {
 					lineNumbers: true,
 					lineWrapping: true,
 					lint: true,
+					smartIndent: true,
+					foldGutter: true,
+					gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+					autoCloseTags: true,
+					matchBrackets: true,
+					autoCloseBrackets: true,
+					extraKeys: {
+						"Ctrl-Space": "autocomplete",
+					},
 				}}
 			/>
 		</div>
