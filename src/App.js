f100c32e4acf6015e4d74ff080bc561cb8ecff90
Rahul Anand Sahu <sahurahul442001@gmail.com>
2021-09-10 19:12:57 +0530

    added gh-pages deploy commands, added desktop only support configs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { isMobile, isTablet } from "react-device-detect";
 import Home from "./components/Home/home.component";
+import PatchTemplate from "./components/PatchWeb/patch.component";
 function App() {
 	return (
 		<div className="App">
-			<Home />
+			{!isMobile && !isTablet ? <Home /> : <PatchTemplate />}
 		</div>
 	);
 }
---
+++ src/components/PatchWeb/patch.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./patch.styles.css";
+
+const PatchTemplate = () => {
+	return (
+		<div className="patch-container">
+			<h1 className="patch-title">
+				Sorry, currently the webpage is only supported in Desktop !
+			</h1>
+		</div>
+	);
+};
+
+export default PatchTemplate;
