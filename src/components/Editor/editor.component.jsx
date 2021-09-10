285619abb4bfc8fc2c0f65eb8c14520af0442b47
Rahul Anand Sahu <sahurahul442001@gmail.com>
2021-09-10 14:12:25 +0530

    created code playground with editable options, and wensite rendereing using CodeMirror

--- src/components/Editor/editor.component.jsx
+++ src/components/Editor/editor.component.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "./editor.styles.css";
+import { Controlled as CodeMirror } from "react-codemirror2";
+require("codemirror/mode/xml/xml");
+require("codemirror/mode/javascript/javascript");
+require("codemirror/mode/css/css");
 
-const Editor = ({ count }) => {
+const Editor = (props) => {
+	const { language, displayName, onChange, value } = props;
+
+	const handleChange = (editor, data, value) => {
+		onChange(value);
+	};
+
 	return (
 		<div id="editor">
-			<h1>{`Editor Panel-${count}`}</h1>
+			<div className="editor__title">{displayName}</div>
+			<CodeMirror
+				onBeforeChange={handleChange}
+				value={value}
+				className="CodeMirror"
+				options={{
+					mode: language,
+					theme: "yonce",
+					lineNumbers: true,
+					lineWrapping: true,
+					lint: true,
+				}}
+			/>
 		</div>
 	);
 };
--- src/components/Home/home.component.jsx
+++ src/components/Home/home.component.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import "./home.styles.css";
 import Editor from "../Editor/editor.component";
+
 function Home() {
+	const [html, setHtml] = useState("");
+	const [css, setCss] = useState("");
+	const [js, setJs] = useState("");
+
+	let srcDoc = `
+	<html>
+	<body>${html}</body>
+	<style>${css}</style>
+	<script>${js}</script>
+	</html>
+	`;
+
 	return (
-		<section id="home">
+		<div id="home">
 			<div className="panel top-panel">
-				<Editor count="1" />
-				<Editor count="2" />
-				<Editor count="3" />
+				<Editor
+					language="xml"
+					displayName="HTML"
+					onChange={setHtml}
+					value={html}
+				/>
+				<Editor
+					language="css"
+					displayName="CSS"
+					onChange={setCss}
+					value={css}
+				/>
+				<Editor
+					language="javascript"
+					displayName="JS"
+					onChange={setJs}
+					value={js}
+				/>
 			</div>
-			<div className="panel bottom-panel"></div>
-		</section>
+			<h1 className="bottom-panel-title">OUTPUT PANEL</h1>
+			<iframe
+				srcDoc={srcDoc}
+				title="output"
+				sandbox="allow-scripts"
+				frameBorder="0"
+				width="100%"
+				height="100%"
+			/>
+		</div>
 	);
 }
 
